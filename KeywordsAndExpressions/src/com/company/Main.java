df51b83120c0995dc0239abb7cb0dc37466c53a3
adelapopa <54961717+adelapopa@users.noreply.github.com>
2019-09-09 10:14:27 +0300

    Add files via upload

---
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int int2 = 5;
+	// false, true, null
+        double kilometres = (100 * 1.609344);// the expression component is variables, values and operators
+        int highSchool = 50; //expression
+        if (highSchool == 50) { //(highSchool == 50) is expression
+            System.out.println("this is an expression!"); // "this is an expression" is expression
+        }
+        int score = 100; // score=100 is expression
+        if (score > 99) { // score > 99 is expression
+            System.out.println("you got the high score!"); // string are expression
+            score = 0;  // socre = 0 is expression
+        }
+    }
+}
